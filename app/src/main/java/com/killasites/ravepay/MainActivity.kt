58f9d1dfea1b5c1bb2fd5d90ace357f4bee28994
Anthony <nataanthoni@gmail.com>
2020-03-05 17:54:47 +0300

    New activity pay now instead of default activity

--- app/src/main/java/com/killasites/ravepay/MainActivity.kt
+++ app/src/main/java/com/killasites/ravepay/MainActivity.kt
@@ -1,12 +1,10 @@
 package com.killasites.ravepay
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.EditText
-import com.flutterwave.raveandroid.Meta
-import com.flutterwave.raveandroid.RaveConstants.yes
-import com.flutterwave.raveandroid.RavePayManager
 import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,73 +12,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-            val setCountry = (country)
-            val currency = currency
-            .setEmail(email)
-            .setfName(fName)
-            .setlName(lName)
-            .setNarration(narration)
-            .setPublicKey(publicKey)
-            .setEncryptionKey(encryptionKey)
-            .setTxRef(txRef)
-            .setPhoneNumber(phoneNumber)
-            .acceptAccountPayments(boolean)
-            .acceptCardPayments(boolean)
-            .acceptMpesaPayments(boolean)
-            .acceptAchPayments(boolean)
-            .acceptGHMobileMoneyPayments(boolean)
-            .acceptUgMobileMoneyPayments(boolean)
-            .acceptZmMobileMoneyPayments(boolean)
-            .acceptRwfMobileMoneyPayments(boolean)
-            .acceptUkPayments(boolean)
-            .acceptBankTransferPayments(boolean)
-            .acceptUssdPayments(boolean)
-            .acceptBarterPayments(boolean)
-            .acceptFrancMobileMoneyPayments(boolean)
-            .allowSaveCardFeature(boolean)
-            .onStagingEnv(boolean)
-            .setMeta(List<Meta>)
-            .withTheme(styleId)
-            .isPreAuth(boolean)
-            .setSubAccounts(List<SubAccount>)
-            .shouldDisplayFee(boolean)
-            .showStagingLabel(yes)
-            .initialize()
+           paynow.setOnClickListener {
 
+              intent = Intent(this, PayNow::class.java)
+              startActivity(intent)
+           }
 
-         RavePayManager(this@MainActivity).setAmount(amount2)
-            .setCountry(country)
-            .setCurrency(currency)
-            .setEmail(email)
-            .setfName(fName)
-            .setlName(lName)
-            .setNarration(narration)
-            .setPublicKey(publicKey)
-            .setEncryptionKey(encryptionKey)
-            .setTxRef(txRef)
-            .setPhoneNumber(phoneNumber)
-            .acceptAccountPayments(boolean)
-            .acceptCardPayments(boolean)
-            .acceptMpesaPayments(boolean)
-            .acceptAchPayments(boolean)
-            .acceptGHMobileMoneyPayments(boolean)
-            .acceptUgMobileMoneyPayments(boolean)
-            .acceptZmMobileMoneyPayments(boolean)
-            .acceptRwfMobileMoneyPayments(boolean)
-            .acceptUkPayments(boolean)
-            .acceptBankTransferPayments(boolean)
-            .acceptUssdPayments(boolean)
-            .acceptBarterPayments(boolean)
-            .acceptFrancMobileMoneyPayments(boolean)
-            .allowSaveCardFeature(boolean)
-            .onStagingEnv(boolean)
-            .setMeta(List<Meta>)
-            .withTheme(styleId)
-            .isPreAuth(boolean)
-            .setSubAccounts(List<SubAccount>)
-            .shouldDisplayFee(boolean)
-            .showStagingLabel(yes)
-            .initialize()
     }
 
 
